b4f968f37312fd6fd9bb01374ddf9142d1c0aabe
unknown <dariansung@umail.ucsb.edu>
2020-05-08 19:44:38 -0700

    Cleaned up guest book page

--- src/components/GuestBookForm.js
+++ src/components/GuestBookForm.js
@@ -16,26 +16,28 @@ export class GuestBookForm extends Component {
     }
 
     componentDidMount = () => {
-      if (!firebase.apps.length) {
-        firebase.initializeApp(config);
-      } 
+        if (!firebase.apps.length) {
+            firebase.initializeApp(config);
+        }
     }
 
-    getCurrentDate(){
+    getCurrentDate() {
         let d = new Date();
         let month = d.getMonth() + 1;
         let day = d.getDate();
         let year = d.getFullYear();
         let hour = d.getHours();
         let minutes = d.getMinutes();
+        if (minutes < 10)
+            minutes = "0" + minutes;
         let meridian;
-        if(hour > 11){
+        if (hour > 11) {
             meridian = "PM";
-            if(hour !== 12)
+            if (hour !== 12)
                 hour -= 12;
-        } else{
+        } else {
             meridian = "AM";
-            if(hour === 0)
+            if (hour === 0)
                 hour = 12;
         }
         return month + "/" + day + "/" + year + " " + hour + ":" + minutes + " " + meridian;
@@ -48,29 +50,34 @@ export class GuestBookForm extends Component {
     }
 
     handleError = () => {
-        if(this.state.name.length <= 5){
-            this.setState({errorMessage: "Name must be longer than 5 characters"});
+        if (this.state.name.length <= 5) {
+            this.setState({ errorMessage: "Name must be longer than 5 characters" });
             return true;
         }
-        else if(this.state.name.length >= 20){
-            this.setState({errorMessage: "Name must be less than 19 characters"});
+        else if (this.state.name.length >= 20) {
+            this.setState({ errorMessage: "Name must be less than 19 characters" });
             return true;
         }
-        else if(this.state.message.length <= 15){
-            this.setState({errorMessage: "Message must have more than 15 characters"});
+        else if (this.state.desc.length >= 100) {
+            this.setState({ errorMessage: "Description must be less than 100 characters" });
             return true;
         }
-        else if(this.state.message.length >= 500){
-            this.setState({errorMessage: "Message must be shorter than 500 characters"});
+        else if (this.state.message.length <= 15) {
+            this.setState({ errorMessage: "Message must have more than 15 characters" });
             return true;
         }
+        else if (this.state.message.length >= 500) {
+            this.setState({ errorMessage: "Message must be shorter than 500 characters" });
+            return true;
+        }
+        this.setState({ errorMessage: "" })
         return false;
     }
 
     handleSubmit = (e) => {
         e.preventDefault();
         let hasError = this.handleError();
-        if(hasError) return;
+        if (hasError) return;
 
         let date = this.getCurrentDate();
         let dateMilli = new Date().getTime() * -1;
@@ -97,26 +104,25 @@ export class GuestBookForm extends Component {
 
     render() {
         return (
-            <div className="msg-form">
-                <form onSubmit={this.handleSubmit}>
-                    <p>Name:</p>
-                    <input type="text" name="name" onChange={this.handleChange} value={this.state.name}/>
-                    <p>Brief Description of Yourself:</p>
-                    <input type="text" name="desc" onChange={this.handleChange} value={this.state.desc}/>
-                    <p>Message:</p>
-                    <input type="text" name="message" onChange={this.handleChange} value={this.state.message}/>
-                    <p>Who would you like to see this message?</p>
-                    <select name="sendTo" onChange={this.handleChange} value={this.state.sendTo}>
-                        <option value="owner">Site owner(Darian Sung)</option>
-                        <option value="public">All website visitors</option>
-                    </select>
-                    <p>Email:</p>
-                    <input type="text" name="email" onChange={this.handleChange} value={this.state.email}/>
-                    <p></p>
-                    <button>Submit</button>
-                </form>
-                <p>{this.state.errorMessage}</p>
-            </div>
+            <form className="msg-form" onSubmit={this.handleSubmit}>
+                <h2>Write a message!</h2>
+                <p>Name:</p>
+                <input type="text" name="name" onChange={this.handleChange} value={this.state.name} />
+                <p>Brief Description of Yourself: (optional)</p>
+                <input type="text" name="desc" onChange={this.handleChange} value={this.state.desc} />
+                <p>Message:</p>
+                <textarea rows="4" name="message" onChange={this.handleChange} value={this.state.message} />
+                <p>Who would you like to see this message?</p>
+                <select name="sendTo" onChange={this.handleChange} value={this.state.sendTo}>
+                    <option value="owner">Site owner(Darian Sung)</option>
+                    <option value="public">All website visitors</option>
+                </select>
+                <p>Email: (optional)</p>
+                <input type="text" name="email" onChange={this.handleChange} value={this.state.email} />
+                <p></p>
+                <button className="msg-form-button">Submit</button>
+                <p id="form-error">{this.state.errorMessage}</p>
+            </form>
         )
     }
 }
--- src/components/GuestBookMessage.js
+++ src/components/GuestBookMessage.js
@@ -2,18 +2,23 @@ import React, { Component } from 'react';
 
 export class GuestBookMessage extends Component {
     getDesc = () => {
-        if(this.props.desc){
-            return <p>{this.props.desc}</p>
+        if (this.props.message.desc !== "") {
+            return <p className="gb-msg-desc">{this.props.message.desc}</p>;
         }
     }
 
-    render(){
-        return(
+    render() {
+        if (this.props.message.sendTo === "owner") {
+            return null;
+        }
+        return (
             <div className="gb-msg">
-                <h3>{this.props.message.name}</h3>
-                <p>{this.props.message.date}</p>
-                {this.getDesc()}
-                <p>{this.props.message.message}</p>
+                <div className="gb-msg-header">
+                    <h3>{this.props.message.name}</h3>
+                    {this.getDesc()}
+                </div>
+                <p className="gb-msg-date">{this.props.message.date}</p>
+                <p className="gb-msg-text">{this.props.message.message}</p>
             </div>
         )
     }
